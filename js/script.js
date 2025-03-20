e88705324cecd7a8b7f500a21f21ec8ce158ccfa
Aimar Fernandez <aimar.fernandez1@stu.ocsb.ca>
2025-03-20 17:00:01 +0000

    added js code

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Aimar Fernandez
+// Created on: Feb 2025
 // This file contains the JS functions for index.html
 
-/**
- * Displays an alert with the message "Hello, World!"
- */
 function myButtonClicked() {
-  alert("Hello, World!")
+  // input
+  const base = parseFloat(document.getElementById("base").value)
+  const height = parseFloat(document.getElementById("height").value)
+  // output
+  document.getElementById("answer").innerHTML =
+    "Area is: " + base*height/2 + "cm²"
 }
